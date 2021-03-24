ae1846d5951071099335e2962a9180a47357eb9e
sakirulalamsifat <46572444+sakirulalamsifat@users.noreply.github.com>
2021-03-25 00:30:49 +0600

    Update GetData.js

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -8,7 +8,7 @@ const GetData = ({post}) => {
         
         <div>
             <Link to={'/'}><Button style={{marginBottom:'50px'}} variant='primary'>See Table</Button></Link><br/>
-            <Badge style={{textAlign:'center'}} variant="danger"><h1>{postData}</h1></Badge>
+            <h1>Token is :</h1><Badge style={{textAlign:'center'}} variant="danger"><h1>{postData}</h1></Badge>
             
         </div>
     )
